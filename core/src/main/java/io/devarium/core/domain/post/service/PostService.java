6b23534cf310694369691c4b11854caca2b34484
sillysillman <sillysillyman.cs@gmail.com>
2024-12-10 02:12:16 +0900

    feat: PostService 인터페이스 추가

--- core/src/main/java/io/devarium/core/domain/post/service/PostService.java
+++ core/src/main/java/io/devarium/core/domain/post/service/PostService.java
@@ -2,25 +2,8 @@ package io.devarium.core.domain.post.service;
 
 import io.devarium.core.domain.post.Post;
 import io.devarium.core.domain.post.command.UpsertPostCommand;
-import io.devarium.core.domain.post.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
-public class PostService {
+public interface PostService {
 
-    private final PostRepository postRepository;
-
-    public Post createPost(
-        UpsertPostCommand command
-        // TODO: User user
-    ) {
-        Post post = Post.builder()
-            .title(command.title())
-            .content(command.content())
-            .build();
-
-        return postRepository.save(post);
-    }
+    Post createPost(UpsertPostCommand command);
 }
