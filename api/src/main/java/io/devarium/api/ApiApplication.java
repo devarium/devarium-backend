910b1c9b28f5bd96cca0645ea004be00acf9e266
sillysillman <sillysillyman.cs@gmail.com>
2024-12-10 00:30:32 +0900

    feat: 모듈 Bean 스캔 애너테이션 추가

--- api/src/main/java/io/devarium/api/ApiApplication.java
+++ api/src/main/java/io/devarium/api/ApiApplication.java
@@ -2,8 +2,14 @@ package io.devarium.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "io.devarium.api",
+    "io.devarium.core",
+    "io.devarium.infrastructure"
+})
 public class ApiApplication {
 
     public static void main(String[] args) {
