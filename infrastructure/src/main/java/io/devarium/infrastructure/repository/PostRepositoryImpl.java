3b8240d6660f829140a05a749c2711f2774ce46c
sillysillman <sillysillyman.cs@gmail.com>
2024-12-10 00:34:37 +0900

    feat: PostRepository 구현체 추가
    - 변환 메서드 구현

---
+++ infrastructure/src/main/java/io/devarium/infrastructure/repository/PostRepositoryImpl.java
@@ -0,0 +1,42 @@
+package io.devarium.infrastructure.repository;
+
+import io.devarium.core.domain.post.Post;
+import io.devarium.core.domain.post.repository.PostRepository;
+import io.devarium.infrastructure.entity.PostEntity;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class PostRepositoryImpl implements PostRepository {
+
+    private final PostJpaRepository postJpaRepository;
+
+    @Override
+    public Post save(Post post) {
+        PostEntity entity = convertToEntity(post);
+        PostEntity savedEntity = postJpaRepository.save(entity);
+        return convertToDomain(savedEntity);
+    }
+
+    @Override
+    public Optional<Post> findById(Long id) {
+        return postJpaRepository.findById(id).map(this::convertToDomain);
+    }
+
+    private Post convertToDomain(PostEntity entity) {
+        return Post.builder()
+            .id(entity.getId())
+            .title(entity.getTitle())
+            .content(entity.getContent())
+            .build();
+    }
+
+    private PostEntity convertToEntity(Post post) {
+        return PostEntity.builder()
+            .title(post.getTitle())
+            .content(post.getContent())
+            .build();
+    }
+}
