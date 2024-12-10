c1745f73b2a4f578c01b465cfdddb34596c44e06
soyoung <soyoung.yang94@gmail.com>
2024-12-10 12:49:24 +0900

    fix: 외부 모듈 영속성 애너테이션 추가

--- api/src/main/java/io/devarium/api/ApiApplication.java
+++ api/src/main/java/io/devarium/api/ApiApplication.java
@@ -2,14 +2,18 @@ package io.devarium.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "io.devarium.infrastructure.persistence")
 @ComponentScan(basePackages = {
     "io.devarium.api",
     "io.devarium.core",
     "io.devarium.infrastructure"
 })
+@EnableJpaRepositories(basePackages = "io.devarium.infrastructure.persistence")
 public class ApiApplication {
 
     public static void main(String[] args) {
