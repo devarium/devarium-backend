b14d079c9c7cd054e7693b90b511576bad2977ea
soyoung <soyoung.yang94@gmail.com>
2024-12-10 17:34:56 +0900

    fix: api version 엔드포인트를 application.yaml에서 참조하도록 수정

--- api/src/main/java/io/devarium/api/controller/post/PostController.java
+++ api/src/main/java/io/devarium/api/controller/post/PostController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/posts")
+@RequestMapping("/api/${api.version}/posts")
 @RestController
 public class PostController {
 
