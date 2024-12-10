48e92cbeae87a43df39eb21f2900543d1ccdf8c7
soyoung <soyoung.yang94@gmail.com>
2024-12-10 20:22:49 +0900

    feat: 댓글 CRUD service 로직 구현

--- core/src/main/java/io/devarium/core/domain/comment/service/CommentServiceImpl.java
+++ core/src/main/java/io/devarium/core/domain/comment/service/CommentServiceImpl.java
@@ -17,4 +17,22 @@ public class CommentServiceImpl implements CommentService {
             .build();
         return commentRepository.save(comment);
     }
+
+    @Override
+    public Comment getComment(Long commentId) {
+        return commentRepository.findById(commentId)
+            .orElseThrow(() -> new RuntimeException("comment not found."));
+    }
+
+    @Override
+    public Comment updateComment(Long commentId, UpsertCommentCommand command) {
+        Comment comment = getComment(commentId);
+        comment.updateContent(command.content());
+        return commentRepository.save(comment);
+    }
+
+    @Override
+    public void deleteComment(Long commentId) {
+        commentRepository.deleteById(commentId);
+    }
 }
