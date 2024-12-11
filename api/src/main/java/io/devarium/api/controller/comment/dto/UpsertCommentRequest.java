63ba2bb7bcbcd6ffe0bf7abd13a250b9390f8fb7
soyoung <soyoung.yang94@gmail.com>
2024-12-11 19:54:11 +0900

    chore: UpsertCommentRequest 유효성 검증 메시지 수정

--- api/src/main/java/io/devarium/api/controller/comment/dto/UpsertCommentRequest.java
+++ api/src/main/java/io/devarium/api/controller/comment/dto/UpsertCommentRequest.java
@@ -4,7 +4,7 @@ import io.devarium.core.domain.comment.command.UpsertCommentCommand;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpsertCommentRequest(
-    @NotBlank(message = "'content' must not be blank.") String content
+    @NotBlank(message = "'content' must not be blank") String content
 ) implements UpsertCommentCommand {
 
 }
