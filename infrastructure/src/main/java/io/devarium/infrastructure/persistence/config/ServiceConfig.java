dc5fadc6dceabd92d052f2d081740c94cbc34bf6
sillysillman <sillysillyman.cs@gmail.com>
2024-12-12 01:34:27 +0900

    feat: ProjectService Bean 등록

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/config/ServiceConfig.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/config/ServiceConfig.java
@@ -3,7 +3,11 @@ package io.devarium.infrastructure.persistence.config;
 import io.devarium.core.domain.post.repository.PostRepository;
 import io.devarium.core.domain.post.service.PostService;
 import io.devarium.core.domain.post.service.PostServiceImpl;
+import io.devarium.core.domain.project.repository.ProjectRepository;
+import io.devarium.core.domain.project.service.ProjectService;
+import io.devarium.core.domain.project.service.ProjectServiceImpl;
 import io.devarium.infrastructure.persistence.service.PostServiceDecorator;
+import io.devarium.infrastructure.persistence.service.ProjectServiceDecorator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +17,10 @@ public class ServiceConfig {
     @Bean
     public PostService postService(PostRepository postRepository) {
         return new PostServiceDecorator(new PostServiceImpl(postRepository));
+    }
+
+    @Bean
+    public ProjectService projectService(ProjectRepository projectRepository) {
+        return new ProjectServiceDecorator(new ProjectServiceImpl(projectRepository));
     }
 }
