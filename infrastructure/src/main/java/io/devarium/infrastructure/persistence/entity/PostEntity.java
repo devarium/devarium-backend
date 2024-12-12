3293d9c653955666ee41c105e1a5606499852faf
sillysillman <sillysillyman.cs@gmail.com>
2024-12-12 15:02:20 +0900

    fix: content 제약 조건 추가

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/PostEntity.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/PostEntity.java
@@ -1,6 +1,7 @@
 package io.devarium.infrastructure.persistence.entity;
 
 import io.devarium.core.domain.post.Post;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +24,7 @@ public class PostEntity extends BaseEntity {
 
     private String title;
 
+    @Column(nullable = false)
     private String content;
 
     @Builder
