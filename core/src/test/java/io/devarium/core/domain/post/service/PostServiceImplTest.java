efec81c98528d5dc234d38c8bd3383f1cc1207d7
sillysillman <sillysillyman.cs@gmail.com>
2024-12-12 14:46:17 +0900

    fix: 존재하지 않는 id 테스트 시 PostException 확인하도록 수정

--- core/src/test/java/io/devarium/core/domain/post/service/PostServiceImplTest.java
+++ core/src/test/java/io/devarium/core/domain/post/service/PostServiceImplTest.java
@@ -9,6 +9,7 @@ import static org.mockito.BDDMockito.then;
 
 import io.devarium.core.domain.post.Post;
 import io.devarium.core.domain.post.command.UpsertPostCommand;
+import io.devarium.core.domain.post.exception.PostException;
 import io.devarium.core.domain.post.repository.PostRepository;
 import java.util.Optional;
 import org.junit.jupiter.api.Nested;
@@ -105,8 +106,7 @@ public class PostServiceImplTest {
 
             // when & then
             assertThatThrownBy(() -> postService.getPost(NON_EXISTENT_ID))
-                .isInstanceOf(RuntimeException.class)
-                .hasMessage("Post not found.");
+                .isInstanceOf(RuntimeException.class);
 
             then(postRepository).should().findById(NON_EXISTENT_ID);
         }
@@ -157,8 +157,7 @@ public class PostServiceImplTest {
 
             // when & then
             assertThatThrownBy(() -> postService.updatePost(NON_EXISTENT_ID, command))
-                .isInstanceOf(RuntimeException.class)
-                .hasMessage("Post not found.");
+                .isInstanceOf(PostException.class);
 
             then(postRepository).should().findById(NON_EXISTENT_ID);
             then(postRepository).shouldHaveNoMoreInteractions();
