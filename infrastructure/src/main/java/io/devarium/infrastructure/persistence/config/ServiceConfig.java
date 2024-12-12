82e36f5f8531094f107022aecf82b908e5cd9c15
soyoung <soyoung.yang94@gmail.com>
2024-12-12 17:33:31 +0900

    feat: Reply infrastructure 모듈 구현

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/config/ServiceConfig.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/config/ServiceConfig.java
@@ -6,8 +6,12 @@ import io.devarium.core.domain.comment.service.CommentServiceImpl;
 import io.devarium.core.domain.post.repository.PostRepository;
 import io.devarium.core.domain.post.service.PostService;
 import io.devarium.core.domain.post.service.PostServiceImpl;
+import io.devarium.core.domain.reply.repository.ReplyRepository;
+import io.devarium.core.domain.reply.service.ReplyService;
+import io.devarium.core.domain.reply.service.ReplyServiceImpl;
 import io.devarium.infrastructure.persistence.service.CommentServiceDecorator;
 import io.devarium.infrastructure.persistence.service.PostServiceDecorator;
+import io.devarium.infrastructure.persistence.service.ReplyServiceDecorator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +26,10 @@ public class ServiceConfig {
     @Bean
     public CommentService commentService(CommentRepository commentRepository) {
         return new CommentServiceDecorator(new CommentServiceImpl(commentRepository));
+    }
+
+    @Bean
+    public ReplyService replyService(ReplyRepository replyRepository) {
+        return new ReplyServiceDecorator(new ReplyServiceImpl(replyRepository));
     }
 }
