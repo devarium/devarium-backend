6762db697faf43070fa4b8d7cc08ed8672d39369
sillysillman <sillysillyman.cs@gmail.com>
2024-12-15 22:09:39 +0900

    refactor: 인터페이스명 변경

--- api/src/main/java/io/devarium/api/controller/comment/dto/UpsertCommentRequest.java
+++ api/src/main/java/io/devarium/api/controller/comment/dto/UpsertCommentRequest.java
@@ -1,10 +1,10 @@
 package io.devarium.api.controller.comment.dto;
 
-import io.devarium.core.domain.comment.command.UpsertCommentCommand;
+import io.devarium.core.domain.comment.port.UpsertComment;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpsertCommentRequest(
     @NotBlank(message = "'content' must not be blank") String content
-) implements UpsertCommentCommand {
+) implements UpsertComment {
 
 }
