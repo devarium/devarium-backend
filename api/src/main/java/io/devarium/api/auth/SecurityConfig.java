21eb1b96511b0d36867e4cda998ccade0fc0de22
soyoung <soyoung.yang94@gmail.com>
2024-12-15 17:27:07 +0900

    fix: Security OAuth 설정 제거 및 콜백 함수 허용

--- api/src/main/java/io/devarium/api/auth/SecurityConfig.java
+++ api/src/main/java/io/devarium/api/auth/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,13 +14,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/","/api/v1/auth/google/callback").permitAll()
                 .anyRequest().authenticated()
-            )
-            .oauth2Login(oauth2 -> oauth2
-                .defaultSuccessUrl("/home", true) // 성공 시 리디렉션 URL
-            ); // OAuth 2.0 로그인 지원 활성화
+            );
+//            .oauth2Login(oauth2 -> oauth2
+//                .defaultSuccessUrl("/home", true) // 성공 시 리디렉션 URL
+//                .failureUrl("/login?error=true")  // 로그인 실패 시 URL
+//            ); // OAuth 2.0 로그인 지원 활성화
         return http.build();
     }
 }
