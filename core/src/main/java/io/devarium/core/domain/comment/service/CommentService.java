62af082b92e38dd8108ffac6af535d850d72e828
sillysillman <sillysillyman.cs@gmail.com>
2024-12-15 22:09:51 +0900

    refactor: 파라미터명 변경

--- core/src/main/java/io/devarium/core/domain/comment/service/CommentService.java
+++ core/src/main/java/io/devarium/core/domain/comment/service/CommentService.java
@@ -1,15 +1,15 @@
 package io.devarium.core.domain.comment.service;
 
 import io.devarium.core.domain.comment.Comment;
-import io.devarium.core.domain.comment.command.UpsertCommentCommand;
+import io.devarium.core.domain.comment.port.UpsertComment;
 
 public interface CommentService {
 
-    Comment createComment(UpsertCommentCommand command);
+    Comment createComment(UpsertComment request);
 
     Comment getComment(Long commentId);
 
-    Comment updateComment(Long commentId, UpsertCommentCommand command);
+    Comment updateComment(Long commentId, UpsertComment request);
 
     void deleteComment(Long commentId);
 }
