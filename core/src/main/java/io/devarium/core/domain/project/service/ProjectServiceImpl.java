4997e4231a2f94f1f2b8ccf41101532fd108f7f9
sillysillman <sillysillyman.cs@gmail.com>
2024-12-16 01:30:32 +0900

    fix: 매개변수명 변경

--- core/src/main/java/io/devarium/core/domain/project/service/ProjectServiceImpl.java
+++ core/src/main/java/io/devarium/core/domain/project/service/ProjectServiceImpl.java
@@ -1,7 +1,7 @@
 package io.devarium.core.domain.project.service;
 
 import io.devarium.core.domain.project.Project;
-import io.devarium.core.domain.project.command.UpsertProjectCommand;
+import io.devarium.core.domain.project.command.UpsertProject;
 import io.devarium.core.domain.project.exception.ProjectErrorCode;
 import io.devarium.core.domain.project.exception.ProjectException;
 import io.devarium.core.domain.project.repository.ProjectRepository;
@@ -13,11 +13,11 @@ public class ProjectServiceImpl implements ProjectService {
     private final ProjectRepository projectRepository;
 
     @Override
-    public Project createProject(UpsertProjectCommand command) {
+    public Project createProject(UpsertProject request) {
         Project project = Project.builder()
-            .name(command.name())
-            .description(command.description())
-            .skills(command.skills())
+            .name(request.name())
+            .description(request.description())
+            .skills(request.skills())
             .build();
         return projectRepository.save(project);
     }
@@ -29,11 +29,11 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
-    public Project updateProject(Long projectId, UpsertProjectCommand command) {
+    public Project updateProject(Long projectId, UpsertProject request) {
         Project project = getProject(projectId);
-        project.updateName(command.name());
-        project.updateDescription(command.description());
-        project.updateSkills(command.skills());
+        project.updateName(request.name());
+        project.updateDescription(request.description());
+        project.updateSkills(request.skills());
         return projectRepository.save(project);
     }
 
