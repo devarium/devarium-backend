cdb1e4911a11ec0e06343b5629048705b20524f7
Kang Hyun Ji / Qwen <qwen0524@gmail.com>
2024-12-16 14:11:03 +0900

    feat: Jwt 설정 클래스인 JwtConfig,JwtProperties 작성
    - JwtConfig: JwtProperties를 통해 JWT 서명 키를 생성하고 Bean으로 등록하는 설정 클래스
    - JwtProperties: application.yml 에서 값을 읽어오는 설정 클래스

---
+++ infrastructure/src/main/java/io/devarium/infrastructure/security/jwt/properties/JwtProperties.java
@@ -0,0 +1,17 @@
+package io.devarium.infrastructure.security.jwt.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private String secret;
+    private long accessTokenExpiration;
+    private long refreshTokenExpiration;
+}
+
