e2b64177414b7a23089d0048dac6b82a0b08a34f
Kang Hyun Ji / Qwen <qwen0524@gmail.com>
2024-12-17 10:53:45 +0900

    feat: core에서 Authority 사용을 위해 UserRole을 GrantedAuthority 구현체로 만듦

--- core/src/main/java/io/devarium/core/domain/user/UserRole.java
+++ core/src/main/java/io/devarium/core/domain/user/UserRole.java
@@ -2,12 +2,18 @@ package io.devarium.core.domain.user;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 
 @Getter
 @RequiredArgsConstructor
-public enum UserRole {
+public enum UserRole implements GrantedAuthority {
     ADMIN("ROLE_ADMIN"),
     USER("ROLE_USER");
 
     private final String authority;
+
+    @Override
+    public String getAuthority() {
+        return authority; // 권한 문자열 반환
+    }
 }
