f4e40323ba4dd9347923b15750724a00908e1fa6
Kang Hyun Ji / Qwen <qwen0524@gmail.com>
2024-12-17 10:47:55 +0900

    feat: Redis 사용을 위한 Config, Properties 추가

---
+++ infrastructure/src/main/java/io/devarium/infrastructure/token/RedisProperties.java
@@ -0,0 +1,22 @@
+package io.devarium.infrastructure.token;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@ConfigurationProperties(prefix = "redis")
+public class RedisProperties {
+
+    private final RefreshTokenProperties refreshToken = new RefreshTokenProperties();
+
+    @Getter
+    @Setter
+    public static class RefreshTokenProperties {
+
+        private String prefix;
+        private long expiration;
+    }
+}
+
