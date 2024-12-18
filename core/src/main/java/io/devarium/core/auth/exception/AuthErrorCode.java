4eadae1215ac77d894680366d3c823a60c929b51
sillysillman <sillysillyman.cs@gmail.com>
2024-12-18 16:08:31 +0900

    feat: Google 관련 에러 메시지 추가

--- core/src/main/java/io/devarium/core/auth/exception/AuthErrorCode.java
+++ core/src/main/java/io/devarium/core/auth/exception/AuthErrorCode.java
@@ -8,9 +8,21 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AuthErrorCode {
     ACCESS_TOKEN_IS_NULL(HttpStatus.BAD_REQUEST, "Access Token is null"),
-    GOOGLE_ACCESS_TOKEN_IS_NULL(HttpStatus.BAD_REQUEST, "Google Access Token is null"),
     GITHUB_ACCESS_TOKEN_IS_NULL(HttpStatus.BAD_REQUEST, "Github Access Token is null"),
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "User not found"),
+
+    GOOGLE_TOKEN_REQUEST_FAILED(
+        HttpStatus.BAD_REQUEST,
+        "Failed to request Google access token: %s"
+    ),
+    GOOGLE_INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Google access token"),
+    GOOGLE_USER_INFO_REQUEST_FAILED(
+        HttpStatus.BAD_REQUEST,
+        "Failed to request Google user info: %s"
+    ),
+    GOOGLE_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Google server error occurred: %s"),
+    GOOGLE_INVALID_CODE(HttpStatus.BAD_REQUEST, "Invalid authorization code"),
+
     FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "forbidden access"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "invalid token"),
     UNAUTHENTICATED_USER(HttpStatus.UNAUTHORIZED, "unauthenticated user attempt");
