5c537fa7d33406e43f07c583f4b5ee8425d39c87
Kang Hyun Ji / Qwen <qwen0524@gmail.com>
2024-12-18 10:31:38 +0900

    feat: 에러코드 추가

--- core/src/main/java/io/devarium/core/auth/exception/AuthErrorCode.java
+++ core/src/main/java/io/devarium/core/auth/exception/AuthErrorCode.java
@@ -10,8 +10,11 @@ public enum AuthErrorCode {
     ACCESS_TOKEN_IS_NULL(HttpStatus.BAD_REQUEST, "Access Token is null"),
     GOOGLE_ACCESS_TOKEN_IS_NULL(HttpStatus.BAD_REQUEST, "Google Access Token is null"),
     GITHUB_ACCESS_TOKEN_IS_NULL(HttpStatus.BAD_REQUEST, "Github Access Token is null"),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "User not found");
-    
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "User not found"),
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "forbidden access"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "invalid token"),
+    UNAUTHENTICATED_USER(HttpStatus.UNAUTHORIZED, "unauthenticated user attempt");
+
     private final HttpStatus status;
     private final String message;
 
