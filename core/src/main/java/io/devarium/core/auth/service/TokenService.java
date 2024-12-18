aa2c0289275df39911c7681a5288347170daf2f8
Kang Hyun Ji / Qwen <qwen0524@gmail.com>
2024-12-18 10:29:44 +0900

    feat: refresh를 위한 TokenService와 구현체인 JwtTokenService 작성

--- core/src/main/java/io/devarium/core/auth/service/TokenService.java
+++ core/src/main/java/io/devarium/core/auth/service/TokenService.java
@@ -5,5 +5,9 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 
 public interface TokenService {
+
     Token generateTokens(String username, Collection<? extends GrantedAuthority> authorities);
+
+    Token refreshTokens(String refreshToken);
+
 }
