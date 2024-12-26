e730ca8e65a6fa44c107100add599d61a9b267a6
soyoung <soyoung.yang94@gmail.com>
2024-12-26 16:47:14 +0900

    refactor: getCommentsByPostId를 Postcontroller로 이동 - RESTful 설계 원칙에 따라 계층 관계 표현

--- api/src/main/java/io/devarium/api/controller/post/PostController.java
+++ api/src/main/java/io/devarium/api/controller/post/PostController.java
@@ -1,12 +1,19 @@
 package io.devarium.api.controller.post;
 
+import io.devarium.api.common.dto.PagedListResponse;
 import io.devarium.api.common.dto.SingleItemResponse;
 import io.devarium.api.controller.post.dto.PostResponse;
 import io.devarium.api.controller.post.dto.UpsertPostRequest;
+import io.devarium.core.domain.comment.Comment;
+import io.devarium.core.domain.comment.service.CommentService;
 import io.devarium.core.domain.post.Post;
 import io.devarium.core.domain.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+    private final CommentService commentService;
 
     @PostMapping
     public ResponseEntity<SingleItemResponse<PostResponse>> createPost(
@@ -44,6 +52,16 @@ public class PostController {
         PostResponse response = PostResponse.from(post);
 
         return ResponseEntity.ok(SingleItemResponse.from(response));
+    }
+
+    @GetMapping("/{postId}/comments")
+    public ResponseEntity<PagedListResponse<Comment>> getCommentsByPostId(
+        @PathVariable Long postId,
+        @PageableDefault(size = Comment.DEFAULT_PAGE_SIZE, sort = "createdAt", direction = Direction.ASC) Pageable pageable
+    ) {
+        Page<Comment> comments = commentService.getCommentsByPostId(postId, pageable);
+
+        return ResponseEntity.ok(PagedListResponse.from(comments));
     }
 
     @PutMapping("/{postId}")
