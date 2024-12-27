2a1dd4b6002b80b6d92a0549022e7983f7b5fb4a
sillysillman <sillysillyman.cs@gmail.com>
2024-12-27 15:10:47 +0900

    refactor: 매개변수 순서 변경

--- api/src/main/java/io/devarium/api/controller/comment/dto/CommentResponse.java
+++ api/src/main/java/io/devarium/api/controller/comment/dto/CommentResponse.java
@@ -3,14 +3,14 @@ package io.devarium.api.controller.comment.dto;
 import io.devarium.core.domain.comment.Comment;
 import java.time.Instant;
 
-public record CommentResponse(Long commentId, Instant createdAt, String content, Long postId) {
+public record CommentResponse(Long commentId, String content, Long postId, Instant createdAt) {
 
     public static CommentResponse from(Comment comment) {
         return new CommentResponse(
             comment.getId(),
-            comment.getCreatedAt(),
             comment.getContent(),
-            comment.getPostId()
+            comment.getPostId(),
+            comment.getCreatedAt()
         );
     }
 }
--- api/src/main/java/io/devarium/api/controller/reply/dto/ReplyResponse.java
+++ api/src/main/java/io/devarium/api/controller/reply/dto/ReplyResponse.java
@@ -3,14 +3,14 @@ package io.devarium.api.controller.reply.dto;
 import io.devarium.core.domain.reply.Reply;
 import java.time.Instant;
 
-public record ReplyResponse(Long replyId, Instant createdAt, String content, Long commentId) {
+public record ReplyResponse(Long replyId, String content, Long commentId, Instant createdAt) {
 
     public static ReplyResponse from(Reply reply) {
         return new ReplyResponse(
             reply.getId(),
-            reply.getCreatedAt(),
             reply.getContent(),
-            reply.getCommentId()
+            reply.getCommentId(),
+            reply.getCreatedAt()
         );
     }
 }
