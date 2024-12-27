bd613a7bd913d1aae8fa03a987e14a3e4f41dd42
sillysillman <sillysillyman.cs@gmail.com>
2024-12-27 23:36:16 +0900

    feat: 응답 DTO에 userId 추가

--- api/src/main/java/io/devarium/api/controller/comment/dto/CommentResponse.java
+++ api/src/main/java/io/devarium/api/controller/comment/dto/CommentResponse.java
@@ -3,13 +3,20 @@ package io.devarium.api.controller.comment.dto;
 import io.devarium.core.domain.comment.Comment;
 import java.time.Instant;
 
-public record CommentResponse(Long commentId, String content, Long postId, Instant createdAt) {
+public record CommentResponse(
+    Long commentId,
+    String content,
+    Long postId,
+    Long userId,
+    Instant createdAt
+) {
 
     public static CommentResponse from(Comment comment) {
         return new CommentResponse(
             comment.getId(),
             comment.getContent(),
             comment.getPostId(),
+            comment.getUserId(),
             comment.getCreatedAt()
         );
     }
--- api/src/main/java/io/devarium/api/controller/reply/dto/ReplyResponse.java
+++ api/src/main/java/io/devarium/api/controller/reply/dto/ReplyResponse.java
@@ -3,13 +3,20 @@ package io.devarium.api.controller.reply.dto;
 import io.devarium.core.domain.reply.Reply;
 import java.time.Instant;
 
-public record ReplyResponse(Long replyId, String content, Long commentId, Instant createdAt) {
+public record ReplyResponse(
+    Long replyId,
+    String content,
+    Long commentId,
+    Long userId,
+    Instant createdAt
+) {
 
     public static ReplyResponse from(Reply reply) {
         return new ReplyResponse(
             reply.getId(),
             reply.getContent(),
             reply.getCommentId(),
+            reply.getUserId(),
             reply.getCreatedAt()
         );
     }
