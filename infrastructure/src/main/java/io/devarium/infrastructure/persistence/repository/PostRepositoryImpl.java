b00bd8a3db95137b9a0bf3b602d0b3e6132a021c
Kang Hyun Ji / Qwen <qwen0524@gmail.com>
2024-12-28 12:16:23 +0900

    feat: PostRepositoryImpl에 도메인변환, 엔티티변환 메서드 수정

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/PostRepositoryImpl.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/PostRepositoryImpl.java
@@ -5,8 +5,11 @@ import io.devarium.core.domain.post.Post;
 import io.devarium.core.domain.post.exception.PostErrorCode;
 import io.devarium.core.domain.post.exception.PostException;
 import io.devarium.core.domain.post.repository.PostRepository;
+import io.devarium.core.domain.user.exception.UserErrorCode;
+import io.devarium.core.domain.user.exception.UserException;
 import io.devarium.infrastructure.persistence.entity.PostEntity;
 import io.devarium.infrastructure.persistence.entity.QPostEntity;
+import io.devarium.infrastructure.persistence.entity.UserEntity;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,6 +21,7 @@ import org.springframework.stereotype.Repository;
 public class PostRepositoryImpl implements PostRepository {
 
     private final PostJpaRepository postJpaRepository;
+    private final UserJpaRepository userJpaRepository;
     private final JPAQueryFactory queryFactory;
 
     @Override
@@ -51,6 +55,7 @@ public class PostRepositoryImpl implements PostRepository {
             .title(entity.getTitle())
             .content(entity.getContent())
             .createdAt(entity.getCreatedAt())
+            .authorId(entity.getAuthor().getId())
             .build();
     }
 
@@ -62,9 +67,13 @@ public class PostRepositoryImpl implements PostRepository {
             return entity;
         }
 
+        UserEntity author = userJpaRepository.findById(domain.getAuthorId()).orElseThrow(
+            () -> new UserException(UserErrorCode.USER_NOT_FOUND, domain.getAuthorId())
+        );
         return PostEntity.builder()
             .title(domain.getTitle())
             .content(domain.getContent())
+            .author(author)
             .build();
     }
 }
