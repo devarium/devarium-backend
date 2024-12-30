2f0dc0696c4b7e66845b0bb78d47a3963f84a587
sillysillman <sillysillyman.cs@gmail.com>
2024-12-30 12:23:21 +0900

    fix: getUserByEmail에서 예외 처리 대신 null 반환하도록 수정
    - 새로운 사용자 가입 시 에러 발생

--- core/src/main/java/io/devarium/core/domain/user/service/UserServiceImpl.java
+++ core/src/main/java/io/devarium/core/domain/user/service/UserServiceImpl.java
@@ -34,8 +34,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUserByEmail(String email) {
-        return userRepository.findByEmail(email)
-            .orElseThrow(() -> new UserException(UserErrorCode.USER_EMAIL_NOT_FOUND, email));
+        return userRepository.findByEmail(email).orElse(null);
     }
 
     @Override
