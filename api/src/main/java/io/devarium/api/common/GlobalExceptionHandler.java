b3846c5552dfa1de9776ea52047cae0c89fe517c
soyoung <soyoung.yang94@gmail.com>
2025-01-02 18:41:48 +0900

    chore: 의도하지 않은 코드 제거

--- api/src/main/java/io/devarium/api/common/GlobalExceptionHandler.java
+++ api/src/main/java/io/devarium/api/common/GlobalExceptionHandler.java
@@ -21,16 +21,9 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.View;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    private final View error;
-
-    public GlobalExceptionHandler(View error) {
-        this.error = error;
-    }
 
     @ExceptionHandler(PostException.class)
     public ResponseEntity<ErrorResponse> handlePostException(PostException e) {
