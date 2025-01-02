873d243545b2ed82da8661e02574137bb2512024
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-03 01:36:44 +0900

    feat: 사용자 프로필 이미지 변경 메서드 추가

--- core/src/main/java/io/devarium/core/domain/user/service/UserService.java
+++ core/src/main/java/io/devarium/core/domain/user/service/UserService.java
@@ -3,6 +3,7 @@ package io.devarium.core.domain.user.service;
 import io.devarium.core.auth.OAuth2UserInfo;
 import io.devarium.core.domain.user.User;
 import io.devarium.core.domain.user.port.UpdateUser;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
 
@@ -13,6 +14,8 @@ public interface UserService {
     User getByEmail(String email);
 
     User updateUserProfile(UpdateUser request, User user);
+
+    User updateUserProfileImageUrl(MultipartFile image, User user);
 
     void withdraw(User user);
 }
