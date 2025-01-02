e1388cbaf2786afeaa69d8344e9c3e05ee543601
soyoung <soyoung.yang94@gmail.com>
2025-01-02 14:44:21 +0900

    feat: team 도메인 DTO

---
+++ core/src/main/java/io/devarium/core/domain/team/port/CreateMembers.java
@@ -0,0 +1,8 @@
+package io.devarium.core.domain.team.port;
+
+import java.util.List;
+
+public interface CreateMembers {
+
+    List<Long> memberIds();
+}
---
+++ core/src/main/java/io/devarium/core/domain/team/port/DeleteMembers.java
@@ -0,0 +1,8 @@
+package io.devarium.core.domain.team.port;
+
+import java.util.List;
+
+public interface DeleteMembers {
+
+    List<Long> memberIds();
+}
