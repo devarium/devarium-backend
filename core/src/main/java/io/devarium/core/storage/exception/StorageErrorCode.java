49730e8069725d3f352daa806eebe49fa0cf14b3
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-05 04:31:41 +0900

    feat: StorageErrorCode 열거 상수 추가

--- core/src/main/java/io/devarium/core/storage/exception/StorageErrorCode.java
+++ core/src/main/java/io/devarium/core/storage/exception/StorageErrorCode.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum StorageErrorCode {
-    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to upload file"),
-    FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to delete file"),
+    EMPTY_FILE(HttpStatus.BAD_REQUEST, "File is empty"),
+    INVALID_FILENAME(HttpStatus.BAD_REQUEST, "Invalid filename"),
     INVALID_FILE_SIZE(HttpStatus.BAD_REQUEST, "File size exceeds maximum limit"),
-    INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST, "Unsupported file type"),
-    INVALID_FILE_URL(HttpStatus.BAD_REQUEST, "Invalid file URL");
+    INVALID_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "Unsupported image type"),
+    INVALID_PROFILE_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "Profile image must be JPEG or PNG"),
+    INVALID_FILE_URL(HttpStatus.BAD_REQUEST, "Invalid file URL: %s"),
+    FILE_READ_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to read file"),
+    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to upload file"),
+    FILE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to delete file");
 
     private final HttpStatus status;
     private final String message;
