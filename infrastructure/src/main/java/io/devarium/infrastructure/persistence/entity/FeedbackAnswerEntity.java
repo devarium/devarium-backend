41d12ee9f0902761f281b08470c2bfab30b797df
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-07 16:49:29 +0900

    feat: 피드백 QnA 엔티티 필드 추가
    - 제약 조건 추가
    - 연관관계 추가

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/FeedbackAnswerEntity.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/FeedbackAnswerEntity.java
@@ -1,9 +1,15 @@
 package io.devarium.infrastructure.persistence.entity;
 
+import io.devarium.core.domain.feedback.answer.Rating;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,4 +24,17 @@ public class FeedbackAnswerEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private Rating rating;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "feedback_question_id", nullable = false)
+    private FeedbackQuestionEntity feedbackQuestion;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 }
--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/FeedbackQuestionEntity.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/FeedbackQuestionEntity.java
@@ -1,9 +1,16 @@
 package io.devarium.infrastructure.persistence.entity;
 
+import io.devarium.core.domain.feedback.question.QuestionType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,4 +25,18 @@ public class FeedbackQuestionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private QuestionType questionType;
+
+    @Column(nullable = false)
+    private boolean required;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "project_id", nullable = false)
+    private ProjectEntity project;
 }
