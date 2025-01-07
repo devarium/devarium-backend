cc82266c9afa299346cb83c16b3986af667df2c2
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-08 02:47:29 +0900

    chore: 불필요한 인터페이스 제거

--- core/src/main/java/io/devarium/core/domain/user/port/OAuth2Client.java
+++
@@ -1,8 +0,0 @@
-package io.devarium.core.domain.user.port;
-
-import io.devarium.core.domain.user.OAuth2UserInfo;
-
-public interface OAuth2Client {
-
-    OAuth2UserInfo getUserInfo(String code);
-}
