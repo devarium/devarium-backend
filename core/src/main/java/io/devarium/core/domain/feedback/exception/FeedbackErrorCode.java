265bb2bd767d142127f2748675eccde6742e887f
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-09 02:26:51 +0900

    feat: 피드백 도메인 에러 코드 추가

---
+++ core/src/main/java/io/devarium/core/domain/feedback/exception/FeedbackErrorCode.java
@@ -0,0 +1,18 @@
+package io.devarium.core.domain.feedback.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum FeedbackErrorCode {
+    QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "Question not found with id: %d");
+
+    private final HttpStatus status;
+    private final String message;
+
+    public String getMessage(Object... args) {
+        return String.format(message, args);
+    }
+}
