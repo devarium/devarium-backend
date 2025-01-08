265e31b8501f71fa8030be42f3761db1c5ec5fe0
soyoung <soyoung.yang94@gmail.com>
2025-01-08 12:54:48 +0900

    fix: delete http 요청 메서드가 put으로 잘못 표기된 점 수정

--- api/src/main/java/io/devarium/api/controller/member/MemberController.java
+++ api/src/main/java/io/devarium/api/controller/member/MemberController.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -86,7 +87,7 @@ public class MemberController {
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/{teamId}/members")
+    @DeleteMapping("/{teamId}/members")
     public ResponseEntity<Void> deleteMembers(
         @PathVariable Long teamId,
         @Valid @RequestBody DeleteMembersRequest request,
