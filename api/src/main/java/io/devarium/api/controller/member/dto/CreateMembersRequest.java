2edb6d0ded1e162c8f84218c6a8f7fa020ef2f98
soyoung <soyoung.yang94@gmail.com>
2025-01-08 16:46:36 +0900

    feat: dto 유효성 검증 추가

--- api/src/main/java/io/devarium/api/controller/member/dto/CreateMembersRequest.java
+++ api/src/main/java/io/devarium/api/controller/member/dto/CreateMembersRequest.java
@@ -2,10 +2,11 @@ package io.devarium.api.controller.member.dto;
 
 import io.devarium.core.domain.member.MemberRole;
 import io.devarium.core.domain.member.port.CreateMembers;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.Map;
 
 public record CreateMembersRequest(
-    Map<Long, MemberRole> userIdToRole
+    @NotEmpty Map<Long, MemberRole> userIdToRole
 ) implements CreateMembers {
 
 }
--- api/src/main/java/io/devarium/api/controller/member/dto/DeleteMembersRequest.java
+++ api/src/main/java/io/devarium/api/controller/member/dto/DeleteMembersRequest.java
@@ -1,10 +1,11 @@
 package io.devarium.api.controller.member.dto;
 
 import io.devarium.core.domain.member.port.DeleteMembers;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.Set;
 
 public record DeleteMembersRequest(
-    Set<Long> memberIds
+    @NotEmpty Set<Long> memberIds
 ) implements DeleteMembers {
 
 }
