28c1b5af36dd4e43537a15221a8493dc579ea2fe
soyoung <soyoung.yang94@gmail.com>
2025-01-09 01:50:41 +0900

    feat: 멤버 역할에 레벨을 부여하여 계층적으로 표현

--- core/src/main/java/io/devarium/core/domain/member/MemberRole.java
+++ core/src/main/java/io/devarium/core/domain/member/MemberRole.java
@@ -1,6 +1,18 @@
 package io.devarium.core.domain.member;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MemberRole {
-    ADMIN,
-    MEMBER
+    SUPER_ADMIN(11),
+    ADMIN(10),
+    VIEWER(1);
+
+    private final int level;
+
+    public Boolean hasAccess(MemberRole requiredRole) {
+        return this.level >= requiredRole.getLevel();
+    }
 }
