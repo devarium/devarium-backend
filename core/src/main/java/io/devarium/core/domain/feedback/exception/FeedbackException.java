9b278deccee8646c0c9d12709b171166c6deefb4
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-09 20:08:14 +0900

    refactor: 문자열 포맷팅 메시지

--- core/src/main/java/io/devarium/core/domain/feedback/exception/FeedbackException.java
+++ core/src/main/java/io/devarium/core/domain/feedback/exception/FeedbackException.java
@@ -7,8 +7,8 @@ public class FeedbackException extends RuntimeException {
 
     private final FeedbackErrorCode errorCode;
 
-    public FeedbackException(FeedbackErrorCode errorCode) {
-        super(errorCode.getMessage());
+    public FeedbackException(FeedbackErrorCode errorCode, Object... args) {
+        super(errorCode.getMessage(args));
         this.errorCode = errorCode;
     }
 }
