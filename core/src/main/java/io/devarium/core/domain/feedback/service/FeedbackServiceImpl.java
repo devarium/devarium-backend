55bfa75c335a19ddfa841f88a416a6c20a95c8d1
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-09 19:51:35 +0900

    feat: 피드백 질문 생성 메서드 구현

--- core/src/main/java/io/devarium/core/domain/feedback/service/FeedbackServiceImpl.java
+++ core/src/main/java/io/devarium/core/domain/feedback/service/FeedbackServiceImpl.java
@@ -4,6 +4,8 @@ import io.devarium.core.domain.feedback.answer.repository.AnswerRepository;
 import io.devarium.core.domain.feedback.question.Question;
 import io.devarium.core.domain.feedback.question.port.CreateQuestions;
 import io.devarium.core.domain.feedback.question.repository.QuestionRepository;
+import io.devarium.core.domain.project.Project;
+import io.devarium.core.domain.project.service.ProjectService;
 import io.devarium.core.domain.user.User;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ public class FeedbackServiceImpl implements FeedbackService {
 
     private final QuestionRepository questionRepository;
     private final AnswerRepository answerRepository;
+    private final ProjectService projectService;
 
     @Override
     public List<Question> createFeedbackQuestions(
@@ -20,6 +23,19 @@ public class FeedbackServiceImpl implements FeedbackService {
         CreateQuestions request,
         User user
     ) {
-        return null;
+        Project project = projectService.getProject(projectId, user);
+        // TODO: 프로젝트 생성 권한 검증
+
+        List<Question> questions = request.questions().stream()
+            .map(q -> Question.builder()
+                .orderNumber(q.orderNumber())
+                .content(q.content())
+                .type(q.type())
+                .required(q.required())
+                .projectId(projectId)
+                .build())
+            .toList();
+
+        return questionRepository.saveAll(questions);
     }
 }
