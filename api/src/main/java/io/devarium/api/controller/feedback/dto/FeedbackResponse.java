08db176d3d885f633188d7bd7bfd2b93689443dd
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-10 01:47:01 +0900

    feat: 정적 팩터리 메서드 임시 추가

--- api/src/main/java/io/devarium/api/controller/feedback/dto/FeedbackResponse.java
+++ api/src/main/java/io/devarium/api/controller/feedback/dto/FeedbackResponse.java
@@ -1,5 +1,10 @@
 package io.devarium.api.controller.feedback.dto;
 
+import io.devarium.core.domain.feedback.Feedback;
+
 public record FeedbackResponse() {
 
+    public static FeedbackResponse from(Feedback feedback) {
+        return new FeedbackResponse();
+    }
 }
