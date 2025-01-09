1f52ffb7cc2e213e1d6b9fa80ed01b66aecc9e08
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-09 16:18:04 +0900

    feat: 인터페이스 구현 추가

--- api/src/main/java/io/devarium/api/controller/feedback/dto/CreateFeedbackQuestionsRequest.java
+++ api/src/main/java/io/devarium/api/controller/feedback/dto/CreateFeedbackQuestionsRequest.java
@@ -1,6 +1,8 @@
 package io.devarium.api.controller.feedback.dto;
 
 import io.devarium.core.domain.feedback.question.QuestionType;
+import io.devarium.core.domain.feedback.question.port.CreateFeedbackQuestion;
+import io.devarium.core.domain.feedback.question.port.CreateFeedbackQuestions;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -8,14 +10,14 @@ import java.util.List;
 
 public record CreateFeedbackQuestionsRequest(
     @NotNull(message = "'questions' must not be null") List<CreateFeedbackQuestionRequest> questions
-) {
+) implements CreateFeedbackQuestions {
 
     public record CreateFeedbackQuestionRequest(
         @NotBlank(message = "'content' must not be blank") String content,
         @NotNull(message = "'type' must not be null") QuestionType type,
         @NotNull(message = "'required' must not be null") Boolean required,
         @PositiveOrZero(message = "'orderNumber' must be greater than or equal to 0") int orderNumber
-    ) {
+    ) implements CreateFeedbackQuestion {
 
     }
 }
