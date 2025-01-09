15b0c5164f7a729be2e73309886a3811ef1b0062
soyoung <soyoung.yang94@gmail.com>
2025-01-09 16:22:19 +0900

    feat: 팀 리더를 변경할 때 변경에 따른 멤버 역할을 함께 변경

--- api/src/main/java/io/devarium/api/controller/team/dto/UpdateLeaderRequest.java
+++ api/src/main/java/io/devarium/api/controller/team/dto/UpdateLeaderRequest.java
@@ -1,9 +1,17 @@
 package io.devarium.api.controller.team.dto;
 
+import io.devarium.core.domain.member.MemberRole;
+import io.devarium.core.domain.member.port.UpdateMembers;
 import io.devarium.core.domain.team.port.UpdateLeader;
+import java.util.Collections;
+import java.util.Map;
 
 public record UpdateLeaderRequest(
-    Long leaderId
-) implements UpdateLeader {
+    Long leaderId,
+    Map<Long, MemberRole> memberIdToRole
+) implements UpdateLeader, UpdateMembers {
 
+    public UpdateLeaderRequest(Long leaderId) {
+        this(leaderId, Collections.emptyMap());
+    }
 }
--- core/src/main/java/io/devarium/core/domain/team/service/TeamServiceImpl.java
+++ core/src/main/java/io/devarium/core/domain/team/service/TeamServiceImpl.java
@@ -1,5 +1,7 @@
 package io.devarium.core.domain.team.service;
 
+import io.devarium.core.domain.member.MemberRole;
+import io.devarium.core.domain.member.port.UpdateMembers;
 import io.devarium.core.domain.member.service.MemberService;
 import io.devarium.core.domain.team.Team;
 import io.devarium.core.domain.team.exception.TeamErrorCode;
@@ -8,6 +10,7 @@ import io.devarium.core.domain.team.port.UpdateLeader;
 import io.devarium.core.domain.team.port.UpsertTeam;
 import io.devarium.core.domain.team.repository.TeamRepository;
 import io.devarium.core.domain.user.User;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -54,6 +57,13 @@ public class TeamServiceImpl implements TeamService {
         Team team = getTeam(teamId);
         team.validateLeader(user.getId());
         team.updateLeader(request.leaderId());
+
+        UpdateMembers membersRequest = (UpdateMembers) request;
+        Map<Long, MemberRole> members = membersRequest.memberIdToRole();
+        members.clear();
+        members.put(team.getLeaderId(), MemberRole.SUPER_ADMIN);
+        members.put(user.getId(), MemberRole.ADMIN);
+        memberService.updateMembers(teamId, membersRequest, user);
         return teamRepository.save(team);
     }
 
