d9f37d267d989c788a5a7f535d90db50f0bcb732
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-10 03:05:00 +0900

    refactor: 피드백 구조 변경

--- core/src/main/java/io/devarium/core/domain/feedback/Feedback.java
+++ core/src/main/java/io/devarium/core/domain/feedback/Feedback.java
@@ -1,39 +1,14 @@
 package io.devarium.core.domain.feedback;
 
-import io.devarium.core.domain.feedback.answer.Answer;
-import io.devarium.core.domain.feedback.exception.FeedbackErrorCode;
-import io.devarium.core.domain.feedback.exception.FeedbackException;
-import io.devarium.core.domain.feedback.question.Question;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-public class Feedback {
+public record Feedback(Long projectId, List<QuestionWithAnswers> questionAnswers) {
 
-    private final Long projectId;
-    private final Map<Question, Answer> questionAnswers =
-        new TreeMap<>(Comparator.comparing(Question::getOrderNumber));
-
-    @Builder
-    public Feedback(Long projectId, List<Question> questions) {
-        this.projectId = projectId;
-        questions.forEach(q -> questionAnswers.put(q, null));
-    }
-
-    public void addAnswer(Question question, Answer answer) {
-        if (!questionAnswers.containsKey(question)) {
-            throw new FeedbackException(FeedbackErrorCode.QUESTION_NOT_FOUND);
-        }
-        questionAnswers.put(question, answer);
-    }
-
-    public boolean isCompleted() {
-        return questionAnswers.entrySet().stream()
-            .filter(entry -> entry.getKey().isRequired())
-            .allMatch(entry -> entry.getValue() != null);
+    public static Feedback of(Long projectId, List<QuestionWithAnswers> questionAnswers) {
+        List<QuestionWithAnswers> sortedQuestionAnswers = questionAnswers.stream()
+            .sorted(Comparator.comparing(qa -> qa.question().getOrderNumber()))
+            .toList();
+        return new Feedback(projectId, sortedQuestionAnswers);
     }
 }
