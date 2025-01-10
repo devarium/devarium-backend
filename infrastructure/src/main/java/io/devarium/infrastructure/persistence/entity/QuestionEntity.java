a46057c807d2c7d14c1a5f4ea7effbcdfde1ba2e
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-10 15:02:15 +0900

    feat: 엔티티 생성자 및 도메인 변환 메서드 추가

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/QuestionEntity.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/QuestionEntity.java
@@ -1,5 +1,6 @@
 package io.devarium.infrastructure.persistence.entity;
 
+import io.devarium.core.domain.feedback.question.Question;
 import io.devarium.core.domain.feedback.question.QuestionType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -34,7 +36,7 @@ public class QuestionEntity extends BaseEntity {
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private QuestionType questionType;
+    private QuestionType type;
 
     @Column(nullable = false)
     private boolean required;
@@ -42,4 +44,44 @@ public class QuestionEntity extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "project_id", nullable = false)
     private ProjectEntity project;
+
+    @Builder
+    private QuestionEntity(Long id, int orderNumber, String content, QuestionType type,
+        boolean required, ProjectEntity project) {
+        this.id = id;
+        this.orderNumber = orderNumber;
+        this.content = content;
+        this.type = type;
+        this.required = required;
+        this.project = project;
+    }
+
+    public static QuestionEntity fromDomain(Question question, ProjectEntity project) {
+        return QuestionEntity.builder()
+            .id(question.getId())
+            .orderNumber(question.getOrderNumber())
+            .content(question.getContent())
+            .type(question.getType())
+            .required(question.isRequired())
+            .project(project)
+            .build();
+    }
+
+    public Question toDomain() {
+        return Question.builder()
+            .id(id)
+            .orderNumber(orderNumber)
+            .content(content)
+            .type(type)
+            .required(required)
+            .projectId(project.getId())
+            .build();
+    }
+
+    public void update(Question question) {
+        this.orderNumber = question.getOrderNumber();
+        this.content = question.getContent();
+        this.type = question.getType();
+        this.required = question.isRequired();
+    }
 }
