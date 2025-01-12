30774c48387658d8a1516cea866a447912263c62
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-13 04:44:47 +0900

    feat: 리포지토리 오버라이드 메서드 추가

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/AnswerRepositoryImpl.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/AnswerRepositoryImpl.java
@@ -1,7 +1,10 @@
 package io.devarium.infrastructure.persistence.repository;
 
+import io.devarium.core.domain.feedback.answer.Answer;
 import io.devarium.core.domain.feedback.answer.repository.AnswerRepository;
 import jakarta.persistence.EntityManager;
+import java.util.List;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,19 @@ public class AnswerRepositoryImpl implements AnswerRepository {
 
     private final EntityManager entityManager;
     private final AnswerJpaRepository answerJpaRepository;
+
+    @Override
+    public List<Answer> saveAll(List<Answer> answers) {
+        return null;
+    }
+
+    @Override
+    public List<Answer> findAllByQuestionIdIn(List<Long> questionIds) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllByQuestionIdIn(Set<Long> questionIds) {
+        
+    }
 }
--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/QuestionRepositoryImpl.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/QuestionRepositoryImpl.java
@@ -1,7 +1,11 @@
 package io.devarium.infrastructure.persistence.repository;
 
+import io.devarium.core.domain.feedback.question.Question;
 import io.devarium.core.domain.feedback.question.repository.QuestionRepository;
 import jakarta.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +15,24 @@ public class QuestionRepositoryImpl implements QuestionRepository {
 
     private final EntityManager entityManager;
     private final QuestionJpaRepository questionJpaRepository;
+
+    @Override
+    public List<Question> saveAll(List<Question> questions) {
+        return null;
+    }
+
+    @Override
+    public Optional<Question> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Question> findAllByProjectId(Long projectId) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllById(Set<Long> ids) {
+
+    }
 }
