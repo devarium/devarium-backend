6424e37e064db9e8f4729fe6b27cd7978b0eff63
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-13 02:02:42 +0900

    fix: orderNumber 유효성 검사 애너테이션 변경

--- api/src/main/java/io/devarium/api/controller/feedback/dto/CreateQuestionsRequest.java
+++ api/src/main/java/io/devarium/api/controller/feedback/dto/CreateQuestionsRequest.java
@@ -5,7 +5,7 @@ import io.devarium.core.domain.feedback.question.port.CreateQuestion;
 import io.devarium.core.domain.feedback.question.port.CreateQuestions;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 
 public record CreateQuestionsRequest(
@@ -13,7 +13,7 @@ public record CreateQuestionsRequest(
 ) implements CreateQuestions {
 
     public record CreateQuestionRequest(
-        @PositiveOrZero(message = "'orderNumber' must be greater than or equal to 0") int orderNumber,
+        @Positive(message = "'orderNumber' must be greater than 0") int orderNumber,
         @NotBlank(message = "'content' must not be blank") String content,
         @NotNull(message = "'type' must not be null") QuestionType type,
         @NotNull(message = "'required' must not be null") Boolean required
