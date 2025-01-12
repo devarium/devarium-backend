32cf21a4b6f93944289eff50418906c69526fe56
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-13 03:08:21 +0900

    refactor: 피드백 QnA 리포지토리 메서드 추가

--- core/src/main/java/io/devarium/core/domain/feedback/question/repository/QuestionRepository.java
+++ core/src/main/java/io/devarium/core/domain/feedback/question/repository/QuestionRepository.java
@@ -1,5 +1,17 @@
 package io.devarium.core.domain.feedback.question.repository;
 
+import io.devarium.core.domain.feedback.question.Question;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 public interface QuestionRepository {
 
+    List<Question> saveAll(List<Question> questions);
+
+    Optional<Question> findById(Long id);
+
+    List<Question> findAllByProjectId(Long projectId);
+
+    void deleteAllById(Set<Long> questionIds);
 }
