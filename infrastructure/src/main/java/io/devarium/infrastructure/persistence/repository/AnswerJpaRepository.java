4582adac1b2fa10dbd535502d33c550ecf00dcfd
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-14 21:19:58 +0900

    feat: JPA 메서드 추가

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/AnswerJpaRepository.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/AnswerJpaRepository.java
@@ -1,8 +1,12 @@
 package io.devarium.infrastructure.persistence.repository;
 
 import io.devarium.infrastructure.persistence.entity.AnswerEntity;
+import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerJpaRepository extends JpaRepository<AnswerEntity, Long> {
 
+    Collection<AnswerEntity> findAllByQuestionIdIn(Collection<Long> questionIds);
+
+    void deleteAllByQuestionIdIn(Collection<Long> questionIds);
 }
