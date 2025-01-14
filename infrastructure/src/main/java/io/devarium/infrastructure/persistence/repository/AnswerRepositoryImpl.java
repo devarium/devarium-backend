97d755a55139f2ac41229cf52910250b2e8ecd21
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-14 21:25:50 +0900

    feat: QnA 리포지토리 메서드 구현

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/AnswerRepositoryImpl.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/AnswerRepositoryImpl.java
@@ -2,6 +2,11 @@ package io.devarium.infrastructure.persistence.repository;
 
 import io.devarium.core.domain.feedback.answer.Answer;
 import io.devarium.core.domain.feedback.answer.repository.AnswerRepository;
+import io.devarium.core.domain.feedback.exception.FeedbackErrorCode;
+import io.devarium.core.domain.feedback.exception.FeedbackException;
+import io.devarium.infrastructure.persistence.entity.AnswerEntity;
+import io.devarium.infrastructure.persistence.entity.QuestionEntity;
+import io.devarium.infrastructure.persistence.entity.UserEntity;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 import java.util.Set;
@@ -17,16 +22,44 @@ public class AnswerRepositoryImpl implements AnswerRepository {
 
     @Override
     public List<Answer> saveAll(List<Answer> answers) {
-        return null;
+        List<AnswerEntity> entities = answers.stream()
+            .map(answer -> {
+                if (answer.getId() != null) {
+                    AnswerEntity entity = answerJpaRepository.findById(answer.getId())
+                        .orElseThrow(() -> new FeedbackException(
+                            FeedbackErrorCode.ANSWER_NOT_FOUND,
+                            answer.getId())
+                        );
+                    entity.update(answer);
+                    return entity;
+                }
+                QuestionEntity question = entityManager.getReference(
+                    QuestionEntity.class,
+                    answer.getQuestionId()
+                );
+                UserEntity user = entityManager.getReference(
+                    UserEntity.class,
+                    answer.getUserId()
+                );
+                return AnswerEntity.fromDomain(answer, question, user);
+            })
+            .toList();
+
+        return answerJpaRepository.saveAll(entities).stream()
+            .map(AnswerEntity::toDomain)
+            .toList();
     }
 
     @Override
     public List<Answer> findAllByQuestionIdIn(List<Long> questionIds) {
-        return null;
+        return answerJpaRepository.findAllByQuestionIdIn(questionIds)
+            .stream()
+            .map(AnswerEntity::toDomain)
+            .toList();
     }
 
     @Override
     public void deleteAllByQuestionIdIn(Set<Long> questionIds) {
-        
+        answerJpaRepository.deleteAllByQuestionIdIn(questionIds);
     }
 }
--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/QuestionRepositoryImpl.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/repository/QuestionRepositoryImpl.java
@@ -1,7 +1,11 @@
 package io.devarium.infrastructure.persistence.repository;
 
+import io.devarium.core.domain.feedback.exception.FeedbackErrorCode;
+import io.devarium.core.domain.feedback.exception.FeedbackException;
 import io.devarium.core.domain.feedback.question.Question;
 import io.devarium.core.domain.feedback.question.repository.QuestionRepository;
+import io.devarium.infrastructure.persistence.entity.ProjectEntity;
+import io.devarium.infrastructure.persistence.entity.QuestionEntity;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
@@ -18,21 +22,44 @@ public class QuestionRepositoryImpl implements QuestionRepository {
 
     @Override
     public List<Question> saveAll(List<Question> questions) {
-        return null;
+        List<QuestionEntity> entities = questions.stream()
+            .map(question -> {
+                if (question.getId() != null) {
+                    QuestionEntity entity = questionJpaRepository.findById(question.getId())
+                        .orElseThrow(() -> new FeedbackException(
+                            FeedbackErrorCode.QUESTION_NOT_FOUND,
+                            question.getId()
+                        ));
+                    entity.update(question);
+                    return entity;
+                }
+                ProjectEntity project = entityManager.getReference(
+                    ProjectEntity.class,
+                    question.getProjectId()
+                );
+                return QuestionEntity.fromDomain(question, project);
+            })
+            .toList();
+
+        return questionJpaRepository.saveAll(entities).stream()
+            .map(QuestionEntity::toDomain)
+            .toList();
     }
 
     @Override
     public Optional<Question> findById(Long id) {
-        return Optional.empty();
+        return questionJpaRepository.findById(id).map(QuestionEntity::toDomain);
     }
 
     @Override
     public List<Question> findAllByProjectId(Long projectId) {
-        return null;
+        return questionJpaRepository.findByProjectId(projectId).stream()
+            .map(QuestionEntity::toDomain)
+            .toList();
     }
 
     @Override
     public void deleteAllById(Set<Long> ids) {
-
+        questionJpaRepository.deleteAllById(ids);
     }
 }
