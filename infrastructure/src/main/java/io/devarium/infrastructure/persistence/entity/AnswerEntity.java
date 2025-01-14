2c97fb3acf578566c5ed53d11b408fee8685e158
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-14 21:19:32 +0900

    feat: 생성자 및 도메인 엔티티 변환 메서드 추가

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/AnswerEntity.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/AnswerEntity.java
@@ -1,5 +1,6 @@
 package io.devarium.infrastructure.persistence.entity;
 
+import io.devarium.core.domain.feedback.answer.Answer;
 import io.devarium.core.domain.feedback.answer.Rating;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -12,6 +13,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -37,4 +39,45 @@ public class AnswerEntity extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
+
+    @Builder
+    private AnswerEntity(
+        Long id,
+        String content,
+        Rating rating,
+        QuestionEntity question,
+        UserEntity user
+    ) {
+        this.id = id;
+        this.content = content;
+        this.rating = rating;
+        this.question = question;
+        this.user = user;
+    }
+
+    public static AnswerEntity fromDomain(Answer answer, QuestionEntity question, UserEntity user) {
+        return AnswerEntity.builder()
+            .id(answer.getId())
+            .content(answer.getContent())
+            .rating(answer.getRating())
+            .question(question)
+            .user(user)
+            .build();
+    }
+
+    public Answer toDomain() {
+        return Answer.builder()
+            .id(id)
+            .content(content)
+            .rating(rating)
+            .questionId(question.getId())
+            .userId(user.getId())
+            .answeredAt(getUpdatedAt())
+            .build();
+    }
+
+    public void update(Answer answer) {
+        this.content = answer.getContent();
+        this.rating = answer.getRating();
+    }
 }
