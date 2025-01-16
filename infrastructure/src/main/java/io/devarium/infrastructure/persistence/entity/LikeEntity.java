5237be6a028f5b8bb7a4b74500342d8ece2c9f52
haeuni00 <leejw6311@naver.com>
2025-01-16 16:23:43 +0900

    feat: LikeEntity uniqueConstraint 추가

--- infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/LikeEntity.java
+++ infrastructure/src/main/java/io/devarium/infrastructure/persistence/entity/LikeEntity.java
@@ -12,16 +12,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "likes")
+@Table(name = "likes",
+    uniqueConstraints = @UniqueConstraint(
+        columnNames = {"user_id", "entity_type", "entity_id"}))
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class LikeEntity extends BaseEntity{
+public class LikeEntity extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
