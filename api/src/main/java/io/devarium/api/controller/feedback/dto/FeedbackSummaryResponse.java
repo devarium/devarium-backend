59af78e056a80340f9447a66a0a0472efb87684f
sillysillyman <sillysillyman.cs@gmail.com>
2025-01-16 17:22:36 +0900

    feat: 피드백 요약 응답 DTO 추가

---
+++ api/src/main/java/io/devarium/api/controller/feedback/dto/FeedbackSummaryResponse.java
@@ -0,0 +1,10 @@
+package io.devarium.api.controller.feedback.dto;
+
+import io.devarium.core.domain.feedback.FeedbackSummary;
+
+public record FeedbackSummaryResponse() {
+
+    public static FeedbackSummaryResponse from(FeedbackSummary feedbackSummary) {
+        return null;
+    }
+}
