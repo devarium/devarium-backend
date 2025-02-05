6c98b4ae3adbc246ea4aefa90e4894985392270e
sillysillyman <sillysillyman.cs@gmail.com>
2025-02-05 14:24:25 +0900

    fix: 리더 생성 메서드 조건문 수정

--- core/src/main/java/io/devarium/core/domain/member/service/MemberServiceImpl.java
+++ core/src/main/java/io/devarium/core/domain/member/service/MemberServiceImpl.java
@@ -55,7 +55,7 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public void createLeader(Long teamId, Long userId) {
-        if (!memberRepository.existsByTeamId(teamId)) {
+        if (memberRepository.existsByTeamId(teamId)) {
             throw new MemberException(MemberErrorCode.FIRST_MEMBER_ONLY, userId, teamId);
         }
         Member member = Member.builder()
