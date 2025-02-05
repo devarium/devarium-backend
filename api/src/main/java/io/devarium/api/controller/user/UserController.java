15898860d6ffc31a0cc6fbe30a5f8909f78c0736
soyoung <soyoung.yang94@gmail.com>
2025-02-06 02:44:18 +0900

    feat: User API 추가 - 내 정보 수정 API 경로 변경 - 타인의 정보 조회 API 추가 - 내 팀 목록 조회 API 추가 - 내 멤버십 목록 조회 API 추가 - 내 팀 참가 요청 목록 조회 API 추가 - 내가 받은 팀 초대 목록 조회 API 추가 - 팀 초대 승인/거절 API 추가

--- api/src/main/java/io/devarium/api/controller/user/UserController.java
+++ api/src/main/java/io/devarium/api/controller/user/UserController.java
@@ -1,21 +1,34 @@
 package io.devarium.api.controller.user;
 
 import io.devarium.api.auth.CustomUserPrincipal;
+import io.devarium.api.common.dto.ListResponse;
 import io.devarium.api.common.dto.SingleItemResponse;
+import io.devarium.api.controller.member.dto.MemberResponse;
+import io.devarium.api.controller.team.dto.TeamResponse;
+import io.devarium.api.controller.teamRequest.dto.TeamRequestResponse;
 import io.devarium.api.controller.user.dto.UpdateUserRequest;
 import io.devarium.api.controller.user.dto.UserResponse;
+import io.devarium.core.domain.member.Member;
+import io.devarium.core.domain.team.Team;
+import io.devarium.core.domain.teamRequest.TeamRequest;
+import io.devarium.core.domain.teamRequest.TeamRequestStatus;
+import io.devarium.core.domain.teamRequest.TeamRequestType;
 import io.devarium.core.domain.user.User;
 import io.devarium.core.domain.user.service.UserService;
 import io.devarium.core.storage.Image;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -37,7 +50,17 @@ public class UserController {
         return ResponseEntity.ok(SingleItemResponse.from(response));
     }
 
-    @PutMapping("/profile")
+    @GetMapping("/{userId}")
+    public ResponseEntity<SingleItemResponse<UserResponse>> getUser(
+        @PathVariable Long userId
+    ) {
+        User user = userService.getUser(userId);
+        UserResponse response = UserResponse.from(user);
+
+        return ResponseEntity.ok(SingleItemResponse.from(response));
+    }
+
+    @PutMapping("/me")
     public ResponseEntity<SingleItemResponse<UserResponse>> updateProfile(
         @Valid @RequestBody UpdateUserRequest request,
         @AuthenticationPrincipal CustomUserPrincipal principal
@@ -48,7 +71,7 @@ public class UserController {
         return ResponseEntity.ok(SingleItemResponse.from(response));
     }
 
-    @PutMapping("/profile/image")
+    @PutMapping("/me/profile-image")
     public ResponseEntity<SingleItemResponse<UserResponse>> updateProfileImage(
         @RequestPart MultipartFile file,
         @AuthenticationPrincipal CustomUserPrincipal principal
@@ -56,6 +79,67 @@ public class UserController {
         User user = userService.updateUserProfileImage(Image.from(file), principal.getUser());
         UserResponse response = UserResponse.from(user);
 
+        return ResponseEntity.ok(SingleItemResponse.from(response));
+    }
+
+    @GetMapping("/me/teams")
+    public ResponseEntity<ListResponse<TeamResponse>> getTeams(
+        @AuthenticationPrincipal CustomUserPrincipal principal
+    ) {
+        List<Team> teams = userService.getTeams(principal.getUser());
+        List<TeamResponse> response = teams.stream()
+            .map(TeamResponse::from).toList();
+        return ResponseEntity.ok(ListResponse.from(response));
+    }
+
+    @GetMapping("/me/memberships")
+    public ResponseEntity<ListResponse<MemberResponse>> getMemberships(
+        @AuthenticationPrincipal CustomUserPrincipal principal
+    ) {
+        List<Member> members = userService.getMemberships(principal.getUser());
+        List<MemberResponse> response = members.stream()
+            .map(MemberResponse::from).toList();
+        return ResponseEntity.ok(ListResponse.from(response));
+    }
+
+    @GetMapping("/me/join-requests")
+    public ResponseEntity<ListResponse<TeamRequestResponse>> getJoinRequests(
+        @AuthenticationPrincipal CustomUserPrincipal principal
+    ) {
+        List<TeamRequest> teamRequests = userService.getTeamRequests(
+            principal.getUser(),
+            TeamRequestType.JOIN_REQUEST
+        );
+        List<TeamRequestResponse> response = teamRequests.stream()
+            .map(TeamRequestResponse::from).toList();
+        return ResponseEntity.ok(ListResponse.from(response));
+    }
+
+    @GetMapping("/me/invitations")
+    public ResponseEntity<ListResponse<TeamRequestResponse>> getInvitations(
+        @AuthenticationPrincipal CustomUserPrincipal principal
+    ) {
+        List<TeamRequest> teamRequests = userService.getTeamRequests(
+            principal.getUser(),
+            TeamRequestType.INVITATION
+        );
+        List<TeamRequestResponse> response = teamRequests.stream()
+            .map(TeamRequestResponse::from).toList();
+        return ResponseEntity.ok(ListResponse.from(response));
+    }
+
+    @PatchMapping("/me/invitations/{teamRequestId}")
+    public ResponseEntity<SingleItemResponse<TeamRequestResponse>> updateInvitation(
+        @PathVariable Long teamRequestId,
+        @RequestParam TeamRequestStatus status,
+        @AuthenticationPrincipal CustomUserPrincipal principal
+    ) {
+        TeamRequest teamRequest = userService.updateInvitation(
+            teamRequestId,
+            status,
+            principal.getUser()
+        );
+        TeamRequestResponse response = TeamRequestResponse.from(teamRequest);
         return ResponseEntity.ok(SingleItemResponse.from(response));
     }
 
