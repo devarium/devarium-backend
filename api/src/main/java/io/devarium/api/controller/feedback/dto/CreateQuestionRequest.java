a9d9dd8e790fd77c9ddfac083d69fa606435767f
sillysillyman <sillysillyman.cs@gmail.com>
2025-02-05 14:52:00 +0900

    feat: 질문 생성 요청 구체화

--- api/src/main/java/io/devarium/api/controller/feedback/dto/CreateQuestionRequest.java
+++ api/src/main/java/io/devarium/api/controller/feedback/dto/CreateQuestionRequest.java
@@ -6,12 +6,40 @@ import io.devarium.core.domain.feedback.question.port.CreateQuestion;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import java.util.List;
 
 public record CreateQuestionRequest(
     @Positive(message = "'orderNumber' must be greater than 0") int orderNumber,
-    @NotBlank(message = "'content' must not be blank") QuestionContent content,
+    @NotNull(message = "'createQuestionContent' must not be null") CreateQuestionContentRequest createQuestionContent,
     @NotNull(message = "'type' must not be null") QuestionType type,
     @NotNull(message = "'required' must not be null") Boolean required
 ) implements CreateQuestion {
 
+    @Override
+    public QuestionContent questionContent() {
+        return new QuestionContent(
+            createQuestionContent.content(),
+            createQuestionContent.choices().stream()
+                .map(choice -> new QuestionContent.Choice(
+                    choice.orderNumber(),
+                    choice.label()
+                ))
+                .toList()
+        );
+    }
+
+    public record CreateQuestionContentRequest(
+        @NotBlank(message = "'questionContent' must not be blank") String content,
+
+        List<CreateChoiceRequest> choices
+    ) implements CreateQuestionContent {
+
+        public record CreateChoiceRequest(
+            @Positive(message = "'orderNumber' must be greater than 0") int orderNumber,
+
+            @NotBlank(message = "'label' must not be blank") String label
+        ) implements CreateChoice {
+
+        }
+    }
 }
--- core/src/main/java/io/devarium/core/domain/feedback/question/port/CreateQuestion.java
+++ core/src/main/java/io/devarium/core/domain/feedback/question/port/CreateQuestion.java
@@ -2,14 +2,29 @@ package io.devarium.core.domain.feedback.question.port;
 
 import io.devarium.core.domain.feedback.question.QuestionContent;
 import io.devarium.core.domain.feedback.question.QuestionType;
+import java.util.List;
 
 public interface CreateQuestion {
 
     int orderNumber();
 
-    QuestionContent content();
+    QuestionContent questionContent();
 
     QuestionType type();
 
     Boolean required();
+
+    interface CreateQuestionContent {
+
+        String content();
+
+        List<? extends CreateChoice> choices();
+    }
+
+    interface CreateChoice {
+
+        int orderNumber();
+
+        String label();
+    }
 }
