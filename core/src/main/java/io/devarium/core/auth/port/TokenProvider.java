4a69f230c24ce3a5ca57f14cddb81c306a4d2fd0
sillysillyman <sillysillyman.cs@gmail.com>
2025-02-06 16:45:52 +0900

    feat: TokenProvider 인터페이스 추가

---
+++ core/src/main/java/io/devarium/core/auth/port/TokenProvider.java
@@ -0,0 +1,19 @@
+package io.devarium.core.auth.port;
+
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+
+public interface TokenProvider {
+
+    String generateAccessToken(String email, Collection<? extends GrantedAuthority> authorities);
+
+    String generateRefreshToken(String email, Collection<? extends GrantedAuthority> authorities);
+
+    String extractEmail(String email);
+
+    Collection<? extends GrantedAuthority> extractAuthorities(String token);
+
+    String resolveToken(String token);
+
+    boolean isTokenValid(String token);
+}
