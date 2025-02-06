1a4c3ca7294abb06cecc989fbb7ce87cf6faa6ca
sillysillyman <sillysillyman.cs@gmail.com>
2025-02-06 11:30:34 +0900

    feat: Feedback 요약 응답 DTO 구체화

--- api/src/main/java/io/devarium/api/controller/feedback/dto/FeedbackSummaryResponse.java
+++ api/src/main/java/io/devarium/api/controller/feedback/dto/FeedbackSummaryResponse.java
@@ -2,9 +2,15 @@ package io.devarium.api.controller.feedback.dto;
 
 import io.devarium.core.domain.feedback.FeedbackSummary;
 
-public record FeedbackSummaryResponse() {
+public record FeedbackSummaryResponse(
+    QuestionResponse question,
+    AnswerResponse answer
+) {
 
     public static FeedbackSummaryResponse from(FeedbackSummary feedbackSummary) {
-        return null;
+        return new FeedbackSummaryResponse(
+            QuestionResponse.from(feedbackSummary.question()),
+            AnswerResponse.from(feedbackSummary.answer())
+        );
     }
 }
