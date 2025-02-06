d782293a0e9dd2c49b6b04745ec79da7c8dcba91
sillysillyman <sillysillyman.cs@gmail.com>
2025-02-06 11:28:53 +0900

    feat: Feedback 응답 DTO 구체화

--- api/src/main/java/io/devarium/api/controller/feedback/dto/FeedbackResponse.java
+++ api/src/main/java/io/devarium/api/controller/feedback/dto/FeedbackResponse.java
@@ -1,10 +1,19 @@
 package io.devarium.api.controller.feedback.dto;
 
 import io.devarium.core.domain.feedback.Feedback;
+import java.util.List;
 
-public record FeedbackResponse() {
+public record FeedbackResponse(
+    QuestionResponse question,
+    List<AnswerResponse> answers
+) {
 
     public static FeedbackResponse from(Feedback feedback) {
-        return new FeedbackResponse();
+        return new FeedbackResponse(
+            QuestionResponse.from(feedback.question()),
+            feedback.answers().stream()
+                .map(AnswerResponse::from)
+                .toList()
+        );
     }
 }
