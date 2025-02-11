bddc9be4d082355bf251f714583999b16e475d0e
soyoung <soyoung.yang94@gmail.com>
2025-02-12 03:20:43 +0900

    feat: 에러코드 추가 및 수정

--- core/src/main/java/io/devarium/core/domain/membership/exception/MembershipErrorCode.java
+++ core/src/main/java/io/devarium/core/domain/membership/exception/MembershipErrorCode.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum MembershipErrorCode {
-    MEMBERSHIP_NOT_FOUND(
+    MEMBERSHIPS_NOT_FOUND(
         HttpStatus.NOT_FOUND,
-        "Membership (ID: %d) not found"
+        "Memberships (ID: %s) not found"
     ),
     MEMBERSHIP_NOT_IN_TEAM(
         HttpStatus.FORBIDDEN,
@@ -19,26 +19,21 @@ public enum MembershipErrorCode {
         HttpStatus.FORBIDDEN,
         "Membership (ID: %d) - User (ID: %d) does not have permission to modify team (ID: %d)"
     ),
-    UPDATE_LEADER_ONLY(
+    INVALID_MEMBER_ROLE(
         HttpStatus.FORBIDDEN,
-        "The SUPER_ADMIN role of member (ID: %d) can only be changed using updateLeader"
-    ),
-    MEMBER_ROLE_HIERARCHY_VIOLATION(
-        HttpStatus.FORBIDDEN,
-        "You do not have permission to modify the roles of members with a higher permission level"
+        "Cannot change to 'LEADER' role"
     ),
     FIRST_MEMBERSHIP_ONLY(
         HttpStatus.FORBIDDEN,
         "User (ID: %d) can only be used when creating the first member of the team (ID: %d) as a leader"
     ),
-    USER_DELETED(
+    INVALID_UPDATE_ENTITY(
         HttpStatus.NOT_FOUND,
-        "User (ID: %d) is deleted"
+        "All requested entities must be updatable entities"
     ),
-    TEAM_DELETED(
-        HttpStatus.NOT_FOUND,
-        "Team (ID: %d) is deleted"
-    );
+    TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "Team (ID: %d) not found"),
+    LEADER_ROLE_UNMODIFIABLE(HttpStatus.FORBIDDEN,
+        "'LEADER' Role of member (ID: %d) cannot be modified");
 
     private final HttpStatus status;
     private final String message;
--- core/src/main/java/io/devarium/core/domain/teamRequest/exception/TeamRequestErrorCode.java
+++ core/src/main/java/io/devarium/core/domain/teamRequest/exception/TeamRequestErrorCode.java
@@ -12,6 +12,20 @@ public enum TeamRequestErrorCode {
     TEAM_REQUEST_NOT_FOUND_BY_USER(
         HttpStatus.NOT_FOUND,
         "Team request (ID: %d) not found by User (ID: %d)"
+    ),
+    MEMBERSHIP_ALREADY_EXISTS(
+        HttpStatus.FORBIDDEN,
+        "User (ID: %d) already has a membership (ID: %d) in the team (ID: %d)"
+    ),
+    MEMBERSHIPS_ALREADY_EXIST(
+        HttpStatus.FORBIDDEN,
+        "Users (ID: %s) already have memberships in the team (ID: %d)"
+    ),
+    TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "Team (ID: %d) not found"),
+    USERS_NOT_FOUND(HttpStatus.NOT_FOUND, "Users (ID: %s) not found"),
+    TEAM_REQUEST_ALREADY_ACCEPTED(
+        HttpStatus.BAD_REQUEST,
+        "Team request (ID: %d) is already in 'ACCEPTED' status, so it cannot be updated to '%s' status"
     );
 
     private final HttpStatus status;
